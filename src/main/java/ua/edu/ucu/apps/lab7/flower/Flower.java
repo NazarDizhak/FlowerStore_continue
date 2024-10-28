8dfcbd3a770f2a1a699cbdb3da4ba2e322cd6358
NazarDizhak <dizhak.pn@ucu.edu.ua>
2024-10-28 21:10:05 +0200

    Initial commit

---
+++ src/main/java/ua/edu/ucu/apps/lab7/flower/Flower.java
@@ -0,0 +1,52 @@
+package ua.edu.ucu.apps.lab7.flower;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Setter
+@Getter
+public class Flower {
+    private double price;
+    private FlowerSpec flowerSpec;
+
+    public Flower() {
+        this.price = 0;
+        this.flowerSpec = new FlowerSpec();
+    }
+
+    public Flower(Flower flower) {
+        this.price = flower.price;
+        this.flowerSpec = flower.flowerSpec;
+    }
+
+    public String getColor() {
+        return flowerSpec.getColor().toString();
+    }
+
+    public double getSepalLength() {
+        return flowerSpec.getSepalLength();
+    }
+
+    public String getFlowerType() {
+        return flowerSpec.getFlowerType().toString();
+    }
+
+    public boolean matches(FlowerSpec otherSpec) {
+        return flowerSpec.matches(otherSpec);
+    }
+
+    public void setColor(FlowerColor color) {
+        flowerSpec.setColor(color);
+    }
+
+    public void setSepalLength(double sepalLength) {
+        flowerSpec.setSepalLength(sepalLength);
+    }
+
+    public void setFlowerType(FlowerType flowerType) {
+        flowerSpec.setFlowerType(flowerType);
+    }
+
+}
---
+++ src/main/java/ua/edu/ucu/apps/lab7/flower/FlowerSpec.java
@@ -0,0 +1,34 @@
+package ua.edu.ucu.apps.lab7.flower;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@AllArgsConstructor
+@Setter
+public class FlowerSpec {
+    private FlowerColor color;
+    private double sepalLength;
+    private FlowerType flowerType;
+
+    public FlowerSpec() {
+        this.color = null;
+        this.sepalLength = 0;
+        this.flowerType = null;
+    }
+
+    public boolean matches(FlowerSpec otherSpec) {
+        if (color != otherSpec.color) {
+            return false;
+        }
+        if (flowerType != otherSpec.flowerType) {
+            return false;
+        }
+        if (sepalLength != otherSpec.sepalLength) {
+            return false;
+        }
+        return true;
+    }
+
+}
